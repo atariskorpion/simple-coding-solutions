a8c8938b0fd00488acc19bbbe95d2ddbcf6e4fa5
atariskorpion <skoepp.mobile@gmail.com>
2017-03-07 15:48:47 +0100

    Add js bubble sort.

---
+++ sorting/05_bubbleSort.js
@@ -0,0 +1,21 @@
+function insertionSort (A = []) {
+
+  var tempValue;
+  var flag = 0;
+
+  for (k = 1; k < A.length; k++) {
+    flag = 0;
+    for (i = 0; i < A.length - k; i++) {
+      if (A[i+1] < A[i]) {
+        tempValue = A[i+1];
+        A[i+1] = A[i];
+        A[i] = tempValue;
+        flag += 1;
+      }
+    }
+    if (flag === 0) {
+      break;
+    }
+  }
+  return A;
+}
