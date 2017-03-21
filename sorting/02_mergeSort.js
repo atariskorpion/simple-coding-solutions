ffd1d65ed56ea8feb4f46f6b42c024b3b29c4990
atariskorpion <skoepp.mobile@gmail.com>
2017-03-21 09:57:24 +0100

    Add js quick sort.

---
+++ sorting/02_mergeSort.js
@@ -0,0 +1,49 @@
+function mergeSort (A = []) {
+  if (A.length <= 1) {
+    return A;
+  }
+
+  let mid = Math.floor(A.length / 2);
+
+  let L = [];
+  for(let l = 0; l < mid; l++) {
+    L[l] = A[l];
+  }
+
+  let R = [];
+	for(let m = mid; m < A.length; m++) {
+    R[m - mid] = A[m];
+  }
+
+  L = mergeSort(L);
+  R = mergeSort(R);
+
+  let i = 0;
+  let j = 0;
+  let k = 0;
+
+  while (i < L.length && j < R.length) {
+    if (L[i] < R[j]) {
+      A[k] = L[i];
+      i++;
+    } else {
+      A[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < L.length) {
+    A[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < R.length) {
+    A[k] = R[j];
+    j++;
+    k++;
+  }
+
+  return A;
+}
