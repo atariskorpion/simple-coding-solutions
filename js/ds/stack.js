780f4e0bc5bdc03ecccd228c1398ebfd3a068517
atariskorpion <skoepp.mobile@gmail.com>
2017-07-12 08:52:24 +0200

    Add simple Stack.

---
+++ js/ds/stack.js
@@ -0,0 +1,27 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  isEmpty() {
+    return this.items === [];
+  }
+
+  push(item = undefined) {
+    if (item) {
+      this.items.push(item);
+    }
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length-1];
+  }
+
+  size() {
+    return this.items.length;
+  }
+}
