ec261fdaf41bd91558e4df143be8dfd9940bde81
atariskorpion <skoepp.mobile@gmail.com>
2018-07-26 09:41:19 +0200

    Rewrite bubble sort.

--- py/sorting/05_bubbleSort.py
+++ py/sorting/05_bubbleSort.py
@@ -1,19 +1,23 @@
 def bubbleSort(A):
+    for k in range(len(A)-1, 0, -1):
+        for i in range(k):
+            if A[i] > A[i+1]:
+                tempValue = A[i]
+                A[i] = A[i+1]
+                A[i+1] = tempValue
 
-    tempValue = 0
+    return A
 
-    for k in range(1, len(A)):
-        flag = 0
-        for i in range(0, len(A) - k):
+print(bubbleSort([54,26,93,17,77,31,44,55,20]))
+
+def bubbleSortReverse(A):
+    for k in range(len(A)-1, 0, -1):
+        for i in range(k):
             if A[i+1] > A[i]:
                 tempValue = A[i+1]
                 A[i+1] = A[i]
                 A[i] = tempValue
-                flag += 1
-
-        if flag == 0:
-          break
 
     return A
 
-print(bubbleSort([1,4,55,3]))
+print(bubbleSortReverse([54,26,93,17,77,31,44,55,20]))
